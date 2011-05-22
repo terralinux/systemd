9119449135e1fe51f3ef9c0c788eff476b1fe63d
Martin Lee <hellnest.fuah@gmail.com>
2011-05-22 22:58:34 +0800

    added terra in hostname

--- src/hostname-setup.c
+++ src/hostname-setup.c
@@ -34,7 +34,7 @@
 #define FILENAME "/etc/sysconfig/network"
 #elif defined(TARGET_SUSE) || defined(TARGET_SLACKWARE) || defined(TARGET_FRUGALWARE)
 #define FILENAME "/etc/HOSTNAME"
-#elif defined(TARGET_ARCH)
+#elif defined(TARGET_ARCH) || defined(TARGET_TERRA)
 #define FILENAME "/etc/rc.conf"
 #elif defined(TARGET_GENTOO)
 #define FILENAME "/etc/conf.d/hostname"
@@ -64,7 +64,7 @@ static int read_and_strip_hostname(const char *path, char **hn) {
 
 static int read_distro_hostname(char **hn) {
 
-#if defined(TARGET_FEDORA) || defined(TARGET_ARCH) || defined(TARGET_GENTOO) || defined(TARGET_ALTLINUX) || defined(TARGET_MANDRIVA) || defined(TARGET_MEEGO)
+#if defined(TARGET_FEDORA) || defined(TARGET_ARCH) || defined(TARGET_GENTOO) || defined(TARGET_ALTLINUX) || defined(TARGET_MANDRIVA) || defined(TARGET_MEEGO) || defined(TARGET_TERRA)
         int r;
         FILE *f;
 
